a29cc4e83e55dc27f7e4274bd21c597bda75f7c8
Tochit <42474056+Tochit@users.noreply.github.com>
2018-08-26 23:42:47 -0700

    Updated

---
+++ hello.py
@@ -0,0 +1,2 @@
+# This is my start back into the real world!
+print("Hello everyone, excited to be here with you all!")
---
+++ input-echo.py
@@ -0,0 +1,8 @@
+fh = open("python.txt", "r")
+
+count = 0
+for line in fh:
+    print(line.strip())
+    count = count + 1
+
+print(count,"Lines")
